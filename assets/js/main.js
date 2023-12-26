be3ce0ee5dd64f6f1c5654e1ce7f620b4c8b5139
Abi Thetchana <muthukumari.m28197@gmail.com>
2023-12-26 14:51:39 +0530

    fix: validate condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,106 @@
+// Import JustValidate file
 import JustValidate from "just-validate";
-const validate = new JustValidate();
+
+// Get form element using getElementById
+const formEl = document.getElementById("courierForm");
+
+const validateForm = new JustValidate(formEl);
+console.log(validateForm);
+
+// validate form using addfield method
+// validate 1st name
+
+validateForm.addField(
+  "#firstName",
+  [
+    {
+      rule: "required",
+    },
+    {
+      rule: "minLength",
+      value: 3,
+    },
+    {
+      rule: "maxLength",
+      value: 25,
+    },
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
+
+// validate 2nd name
+
+validateForm.addField(
+  "#lastName",
+  [
+    {
+      rule: "required",
+    },
+    {
+      rule: "minLength",
+      value: 1,
+    },
+    {
+      rule: "maxLength",
+      value: 25,
+    },
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
+
+// validate mobile number
+
+validateForm.addField(
+  "#mobileNumber",
+  [
+    {
+      rule: "required",
+    },
+    {
+      rule: "number",
+    },
+    {
+      rule: "minLength",
+      value: 10,
+    },
+    {
+      rule: "maxLength",
+      value: 10,
+    },
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
+
+// validate date&time
+
+validateForm.addField(
+  "#dateTime",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
+
+// validate address
+
+validateForm.addField(
+  "#address",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  {
+    errorLabelCssClass: ["form-error"],
+  }
+);
